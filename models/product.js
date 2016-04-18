1f021669179d1c440e1399750a1536d6e5375788
Tyler Hansen <rthanse2@illinois.edu>
2016-04-18 12:45:58 -0500

    updated schemas

--- models/product.js
+++ models/product.js
@@ -9,7 +9,11 @@ var ProductSchema = new mongoose.Schema({
   sold: { type: Boolean, default: false},
   sellerUser: { type: String, default: '' },
   sellerUserName: { type: String, default: ''},
-  dateCreated: {type: Date, default: Date.now }
+  dateCreated: {type: Date, default: Date.now },
+  currentPrice: { type: Number, required: true },
+  usersWatching: { type: [String], default: []},
+  soldToUser: { type: String, default: '' },
+  soldToUserName: { type: String, default: ''},
 });
 
 // Export the Mongoose model
--- models/user.js
+++ models/user.js
@@ -3,9 +3,13 @@ var mongoose = require('mongoose');
 
 // Define our User schema
 var UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  username: { type: String, required: true },
   email: { type: String, required: true, index:{unique: true}},
-  dateCreated: { type: Date, default: Date.now }
+  mobilePhone: { type: String, required: true, index:{unique: true}},
+  dateCreated: { type: Date, default: Date.now },
+  productsSelling: { type: [String], default: []},
+  productsWatching: { type: [String], default: []},
+  productsBought: { type: [String], default: []}
 });
 
 // Export the Mongoose model
