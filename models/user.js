5a256fa8b88a4448d940337de0c2de8c75cb08f0
Tyler Hansen <rthanse2@illinois.edu>
2016-04-18 13:15:53 -0500

    added all API calls for four basic routes

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, index:{unique: true}},
-  mobilePhone: { type: String, required: true, index:{unique: true}},
+  mobilePhone: { type: String, required: true },
   dateCreated: { type: Date, default: Date.now },
   productsSelling: { type: [String], default: []},
   productsWatching: { type: [String], default: []},
