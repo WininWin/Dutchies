e6aca9b1300b728de793a34bdc42dbf1420538c8
Tyler Hansen <rthanse2@illinois.edu>
2016-04-18 11:17:00 -0500

    added json bodyparser

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ var port = process.env.PORT || 4000;
 var allowCrossDomain = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
   next();
 };
 app.use(allowCrossDomain);
@@ -25,6 +26,9 @@ app.use(allowCrossDomain);
 // Use the body-parser package in our application
 app.use(bodyParser.urlencoded({
   extended: true
+}));
+app.use(bodyParser.json({
+	extended: true
 }));
 
 // All our routes will start with /api
