f2e04a1cb06a0a22f64faed60401ecfb38975667
Bolun Qi <bolunqi2@illinois.edu>
2016-04-19 18:17:45 +0000

    schema done

--- models/product.js
+++ models/product.js
@@ -5,11 +5,13 @@ var mongoose = require('mongoose');
 var ProductSchema = new mongoose.Schema({
   name:  { type: String, required: true },
   description: {type: String, default: ''},
+  category:{type:String, required:true},
   reservePrice:  { type: Number, required: true },
   sold: { type: Boolean, default: false},
   sellerUser: { type: String, default: '' },
   sellerUserName: { type: String, default: ''},
   dateCreated: {type: Date, default: Date.now },
+  dateSold:{type:Date},
   currentPrice: { type: Number, required: true },
   usersWatching: { type: [String], default: []},
   soldToUser: { type: String, default: '' },
--- models/user.js
+++ models/user.js
@@ -6,6 +6,17 @@ var UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, index:{unique: true}},
   mobilePhone: { type: String, required: true },
+  card:[{
+  	cardNumber:{type: Number, required: true},
+  	cardName:{type:String,required:true},
+  	cardExpireDate:{type: String, required:true}
+  }],
+  address:[{
+  	addressInfo:{type:String, required:true},
+  	state:{type:String, required:true},
+  	zipcode:{type: Number,required:true}
+  }],
+
   dateCreated: { type: Date, default: Date.now },
   productsSelling: { type: [String], default: []},
   productsWatching: { type: [String], default: []},
