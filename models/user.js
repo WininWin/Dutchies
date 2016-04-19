96fa98862523759c6e740603304ca2334cbeaa67
Tyler Hansen <rthanse2@illinois.edu>
2016-04-19 10:43:52 -0500

    added bcrypt and password to user schema

--- models/user.js
+++ models/user.js
@@ -1,16 +1,27 @@
 // Load required packages
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 // Define our User schema
 var UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  name: { type: String, required: true },
   email: { type: String, required: true, index:{unique: true}},
+  password: { type: String },
   mobilePhone: { type: String, required: true },
   dateCreated: { type: Date, default: Date.now },
   productsSelling: { type: [String], default: []},
   productsWatching: { type: [String], default: []},
   productsBought: { type: [String], default: []}
 });
+
+
+UserSchema.methods.generateHash = function(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+UserSchema.methods.validPassword = function(password) {
+	return bcrypt.compareSync(password, this.password);
+};
 
 // Export the Mongoose model
 module.exports = mongoose.model('User', UserSchema);
